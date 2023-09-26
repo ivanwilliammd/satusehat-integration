4f9a7c6d6006e57494a5ba40aabfadcd77723c24
ivanwilliammd <ivanwilliam.md@gmail.com>
2023-09-26 21:33:29 +0700

    updated integration

--- config/satusehatintegration.php
+++ config/satusehatintegration.php
@@ -6,13 +6,6 @@ return [
      * This is the name of the table that will be created by the migration and
      * used by the Activity model shipped with this package.
      */
-    'token_table_name' => 'satusehat_tokens',
-
-    /*
-     * This is the database connection that will be used by the migration and
-     * the Activity model shipped with this package. In case it's not set
-     * Laravel database.default will be used instead.
-     */
-    'database_connection' => env('ACTIVITY_LOGGER_DB_CONNECTION'),
-
+    'log_table_name' => 'satusehat_log',
+    'token_table_name' => 'satusehat_token'
 ];
--- src/SatusehatIntegrationServiceProvider.php
+++ src/SatusehatIntegrationServiceProvider.php
@@ -5,54 +5,39 @@ namespace Ivanwilliammd\SatusehatIntegration;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
-class ActivitylogServiceProvider extends ServiceProvider
+class SatusehatIntegrationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        // Publish Config
         $this->publishes([
             __DIR__.'/../config/satusehatintegration.php' => config_path('satusehatintegration.php'),
         ], 'config');
 
         $this->mergeConfigFrom(__DIR__.'/../config/satusehatintegration.php', 'satusehatintegration');
 
-        if (! class_exists('CreateActivityLogTable')) {
+        // Publish Migrations for Token
+        if (! class_exists('CreateSatusehatTokenTable')) {
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__.'/../migrations/create_activity_log_table.php.stub' => database_path("/migrations/{$timestamp}_create_activity_log_table.php"),
+                __DIR__.'/../migrations/create_satusehat_token_table.php.stub' => database_path("/migrations/{$timestamp}_create_satusehat_token_table.php"),
+            ], 'migrations');
+        }
+
+        // Publish Migrations for Log
+        if (! class_exists('CreateSatusehatLogTable')) {
+            $timestamp = date('Y_m_d_His', time());
+
+            $this->publishes([
+                __DIR__.'/../migrations/create_satusehat_log_table.php.stub' => database_path("/migrations/{$timestamp}_create_satusehat_log_table.php"),
             ], 'migrations');
         }
     }
 
     public function register()
     {
-        $this->app->bind('command.activitylog:clean', CleanActivitylogCommand::class);
 
-        $this->commands([
-            'command.activitylog:clean',
-        ]);
-
-        $this->app->bind(ActivityLogger::class);
-
-        $this->app->singleton(ActivityLogStatus::class);
     }
 
-    public static function determineActivityModel(): string
-    {
-        $activityModel = config('activitylog.activity_model') ?? ActivityModel::class;
-
-        if (! is_a($activityModel, Activity::class, true)
-            || ! is_a($activityModel, Model::class, true)) {
-            throw InvalidConfiguration::modelIsNotValid($activityModel);
-        }
-
-        return $activityModel;
-    }
-
-    public static function getActivityModelInstance(): ActivityContract
-    {
-        $activityModelClassName = self::determineActivityModel();
-
-        return new $activityModelClassName();
-    }
 }
