e2e756b18c43e438cd0fe2e4048414d34a8e9588
ivanwilliammd <ivanwilliammd@users.noreply.github.com>
2023-10-18 05:35:53 +0000

    Fix styling

--- src/SatusehatIntegrationServiceProvider.php
+++ src/SatusehatIntegrationServiceProvider.php
@@ -44,13 +44,13 @@ class SatusehatIntegrationServiceProvider extends ServiceProvider
 
         // Publish ICD-10 csv data
         $this->publishes([
-            __DIR__.'/../database/seeders/csv/icd10.csv.stub' => database_path("/seeders/csv/icd10.csv"),
+            __DIR__.'/../database/seeders/csv/icd10.csv.stub' => database_path('/seeders/csv/icd10.csv'),
         ], 'icd10');
 
         // Publish Seeder for ICD10
         if (! class_exists('Icd10Seeder')) {
             $this->publishes([
-                __DIR__.'/../database/seeders/Icd10Seeder.php.stub' => database_path("/seeders/Icd10Seeder.php"),
+                __DIR__.'/../database/seeders/Icd10Seeder.php.stub' => database_path('/seeders/Icd10Seeder.php'),
             ], 'icd10');
         }
 
