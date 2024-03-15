86d79e7165e2d376ee46b3532545a3070fd9b886
Syaeful <158563298+SyaefulKai@users.noreply.github.com>
2024-03-15 22:13:28 +0700

    split addStatusHistory method

--- src/FHIR/Encounter.php
+++ src/FHIR/Encounter.php
@@ -11,57 +11,107 @@ class Encounter extends OAuth2Client
 
     public function addRegistrationId($registration_id)
     {
-        $identifier['system'] = 'http://sys-ids.kemkes.go.id/encounter/'.$this->organization_id;
+        $identifier['system'] = 'http://sys-ids.kemkes.go.id/encounter/' . $this->organization_id;
         $identifier['value'] = $registration_id;
 
         $this->encounter['identifier'][] = $identifier;
     }
 
-    public function addStatusHistory($timestamp)
+    private function statusHistoryValidate($arr, $status)
     {
-        // Unset if previously set
-        if (array_key_exists('statusHistory', $this->encounter)) {
-            unset($this->encounter['statusHistory']);
+        
+        $lookingAt = $arr['statusHistory'];
+        foreach ($lookingAt as $data) {
+            if ($data['status'] === $status) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+
+    public function setArrived($timestamp)
+    {
+        if (!isset($this->encounter['statusHistory'])) {
+            $this->encounter['statusHistory'] = [];
         }
 
-        // Arrived
-        if (array_key_exists('arrived', $timestamp)) {
-            $this->encounter['status'] = 'arrived';
-            $this->encounter['period']['start'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['arrived']));
+        $validate = $this->statusHistoryValidate($this->encounter, 'arrived');
 
-            $this->encounter['period']['start'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['arrived']));
-
-            $statusHistory_arrived['status'] = 'arrived';
-            $statusHistory_arrived['period']['start'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['arrived']));
-        } else {
-            return 'arrived is required';
+        if ($validate) {
+            return;
         }
 
-        // In-progress
-        if (array_key_exists('inprogress', $timestamp)) {
-            $this->encounter['status'] = 'in-progress';
+        $statusHistory_arrived =  [
+            'status' => 'arrived',
+            'period' => [
+                'start' => date("Y-m-d\TH:i:sP", strtotime($timestamp)),
+            ],
+        ];
 
-            $statusHistory_inprogress['status'] = 'in-progress';
-            $statusHistory_inprogress['period']['start'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['inprogress']));
-
-            $statusHistory_arrived['period']['end'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['inprogress']));
-        }
-
-        // Finished
-        if (array_key_exists('finished', $timestamp)) {
-            $this->encounter['status'] = 'finished';
-            $this->encounter['period']['end'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['finished']));
-
-            $statusHistory_finished['status'] = 'finished';
-            $statusHistory_finished['period']['start'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['finished']));
-            $statusHistory_finished['period']['end'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['finished']));
-
-            $statusHistory_inprogress['period']['end'] = date("Y-m-d\TH:i:sP", strtotime($timestamp['finished']));
-        }
-
-        // Add all timestamp statusHistory
+        $this->encounter['status'] = 'arrived';
+        $this->encounter['period']['start'] = $statusHistory_arrived['period']['start'];
         $this->encounter['statusHistory'][] = $statusHistory_arrived;
+    }
+
+    public function setInProgress($timestamp_start, $timestamp_end)
+    {
+
+        if (!isset($this->encounter['statusHistory'])) {
+            return 'Please use $this->setArrived first';
+        }
+
+        $validate = $this->statusHistoryValidate($this->encounter, 'in-progress');
+
+        if ($validate) {
+            return;
+        }
+
+        $atomTimestamp = [
+            'start' => date("Y-m-d\TH:i:sP", strtotime($timestamp_start)),
+            'end' => date("Y-m-d\TH:i:sP", strtotime($timestamp_end)),
+        ];
+
+        $statusHistory_inprogress = [
+            'status' => 'in-progress',
+            'period' => [
+                'start' => $atomTimestamp['start'],
+                'end' => $atomTimestamp['end']
+            ]
+        ];
+
+
+        $this->encounter['status'] = 'in-progress';
+        $this->encounter['period']['end'] = $atomTimestamp['end'];
+        $this->encounter['statusHistory'][0]['period']['end'] = $atomTimestamp['start'];
         $this->encounter['statusHistory'][] = $statusHistory_inprogress;
+    }
+
+    public function setFinished($timestamp)
+    {
+
+        if (!isset($this->encounter['statusHistory'])) {
+            return 'Please use $this->setArrived first';
+        }
+
+        $validate = $this->statusHistoryValidate($this->encounter, 'finished');
+
+        if ($validate) {
+            return;
+        }
+
+        $date = date("Y-m-d\TH:i:sP", strtotime($timestamp));
+            
+        $statusHistory_finished = [
+            'status' => 'finished',
+            'period' => [
+                'start' => $date,
+                'end' => $date,
+            ]
+        ];
+        
+        $this->encounter['status'] = 'finished';
+        $this->encounter['period']['end'] = $date;
+        $this->encounter['statusHistory'][0]['period']['end'] = $date;
         $this->encounter['statusHistory'][] = $statusHistory_finished;
     }
 
@@ -101,13 +151,13 @@ class Encounter extends OAuth2Client
 
     public function setSubject($subjectId, $name)
     {
-        $this->encounter['subject']['reference'] = 'Patient/'.$subjectId;
+        $this->encounter['subject']['reference'] = 'Patient/' . $subjectId;
         $this->encounter['subject']['display'] = $name;
     }
 
     public function addParticipant($participantId, $name, $type = 'ATND', $display = 'attender')
     {
-        $participant['individual']['reference'] = 'Practitioner/'.$participantId;
+        $participant['individual']['reference'] = 'Practitioner/' . $participantId;
         $participant['individual']['display'] = $name;
         $participant['type'][]['coding'][] = [
             'system' => 'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
@@ -120,7 +170,7 @@ class Encounter extends OAuth2Client
 
     public function addLocation($locationId, $name)
     {
-        $location['location']['reference'] = 'Location/'.$locationId;
+        $location['location']['reference'] = 'Location/' . $locationId;
         $location['location']['display'] = $name;
 
         $this->encounter['location'][] = $location;
@@ -128,7 +178,7 @@ class Encounter extends OAuth2Client
 
     public function setServiceProvider()
     {
-        $this->encounter['serviceProvider']['reference'] = 'Organization/'.$this->organization_id;
+        $this->encounter['serviceProvider']['reference'] = 'Organization/' . $this->organization_id;
     }
 
     public function addDiagnosis($id, $code, $display = null)
@@ -137,15 +187,15 @@ class Encounter extends OAuth2Client
         $code_check = Icd10::where('icd10_code', $code)->first();
 
         // Handling if incomplete code / display
-        if (! $code_check) {
+        if (!$code_check) {
             return 'Kode ICD-10 invalid';
         }
 
         $display = $display ? $display : $code_check->icd10_en;
 
         // Create Encounter.diagnosis content
-        $diagnosis['condition']['reference'] = 'Condition/'.$id;
-        $diagnosis['condition']['display'] = 'Condition/'.$display;
+        $diagnosis['condition']['reference'] = 'Condition/' . $id;
+        $diagnosis['condition']['display'] = 'Condition/' . $display;
         $diagnosis['use']['coding'][] = [
             'system' => 'http://terminology.hl7.org/CodeSystem/diagnosis-role',
             'code' => 'DD',
@@ -153,7 +203,7 @@ class Encounter extends OAuth2Client
         ];
 
         // Determine ranking
-        if (! array_key_exists('diagnosis', $this->encounter)) {
+        if (!array_key_exists('diagnosis', $this->encounter)) {
             $rank = 1;
         } else {
             $rank = count($this->encounter['diagnosis']) + 1;
@@ -166,32 +216,32 @@ class Encounter extends OAuth2Client
     public function json()
     {
         // Status is required
-        if (! array_key_exists('status', $this->encounter)) {
+        if (!array_key_exists('status', $this->encounter)) {
             return 'Please use encounter->statusHistory([timestamp array]) to add the status';
         }
 
         // Class is required
-        if (! array_key_exists('class', $this->encounter)) {
+        if (!array_key_exists('class', $this->encounter)) {
             return 'Please use encounter->setConsultationMethod($method) to pass the data';
         }
 
         // Subject is required
-        if (! array_key_exists('subject', $this->encounter)) {
+        if (!array_key_exists('subject', $this->encounter)) {
             return 'Please use encounter->setSubject($subjectId, $name) to pass the data';
         }
 
         // Participant is required
-        if (! array_key_exists('participant', $this->encounter)) {
+        if (!array_key_exists('participant', $this->encounter)) {
             return 'Please use encounter->addParticipant($participantId, $name) to pass the data';
         }
 
         // Location is required
-        if (! array_key_exists('location', $this->encounter)) {
+        if (!array_key_exists('location', $this->encounter)) {
             return 'Please use encounter->addLocation($locationId, $name) to pass the data';
         }
 
         // Add default ServiceProvider
-        if (! array_key_exists('serviceProvider', $this->encounter)) {
+        if (!array_key_exists('serviceProvider', $this->encounter)) {
             $this->setServiceProvider();
         }
 
