e1d2bf4687d0d0662db16c4cfc31afe54f697962
ivanwilliammd <ivanwilliammd@users.noreply.github.com>
2024-03-24 10:51:35 +0000

    Fix styling

--- src/FHIR/Exception/FHIRException.php
+++ src/FHIR/Exception/FHIRException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class FHIRException extends Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null)
+    public function __construct($message, $code = 0, ?Exception $previous = null)
     {
         $message = 'FHIR Exception: '.$message;
 
--- src/FHIR/Practitioner.php
+++ src/FHIR/Practitioner.php
@@ -17,54 +17,55 @@ class Practitioner extends OAuth2Client
         }
 
         $this->practitioner = $res->entry ? $res->entry[0]->resource : null;
+
         return $this->practitioner;
     }
 
     public function getId()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->id);
+        return ! $this->practitioner ? null : $this->practitioner->id;
     }
 
     public function getGender()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->gender);
+        return ! $this->practitioner ? null : $this->practitioner->gender;
     }
 
     public function getBirthDate()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->birthDate);
+        return ! $this->practitioner ? null : $this->practitioner->birthDate;
     }
 
     public function getName()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->name[0]->text);
+        return ! $this->practitioner ? null : $this->practitioner->name[0]->text;
     }
 
     public function getQualificationValue()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->qualification[0]->identifier[0]->value);
+        return ! $this->practitioner ? null : $this->practitioner->qualification[0]->identifier[0]->value;
     }
 
     public function getAddressLine()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->address[0]->line[0]);
+        return ! $this->practitioner ? null : $this->practitioner->address[0]->line[0];
     }
 
     public function getCity()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->address[0]->extension[0]->extension[1]->valueCode);
+        return ! $this->practitioner ? null : $this->practitioner->address[0]->extension[0]->extension[1]->valueCode;
     }
 
     public function getVillage()
     {
         // If practitioner is not found, return null
-        return (!$this->practitioner ? null : $this->practitioner->address[0]->extension[0]->extension[3]->valueCode);
+        return ! $this->practitioner ? null : $this->practitioner->address[0]->extension[0]->extension[3]->valueCode;
     }
 }
