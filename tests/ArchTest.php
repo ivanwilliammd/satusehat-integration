9b8d998a6166814ec9c43c31dd53ef48921a9706
ivanwilliammd <ivanwilliam.md@gmail.com>
2024-03-24 17:59:31 +0700

    updated ArchTest.php

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -6,8 +6,6 @@ class ArchTest extends TestCase
 {
     public function testDebuggingFunctionsNotUsed()
     {
-        $this->assertNotContains('dd', ['dd', 'dump', 'ray']);
-        $this->assertNotContains('dump', ['dd', 'dump', 'ray']);
-        $this->assertNotContains('ray', ['dd', 'dump', 'ray']);
+        $this->expect(['dd', 'dump', 'ray'])->not->toBeUsed();
     }
 }
