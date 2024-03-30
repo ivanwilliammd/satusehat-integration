bab927f2c3e9469ac884d36bedc9cc1c958d4661
Syaeful <158563298+SyaefulKai@users.noreply.github.com>
2024-03-30 16:27:57 +0700

    manual UUID generation without external library dependency

--- src/FHIR/Bundle.php
+++ src/FHIR/Bundle.php
@@ -2,10 +2,10 @@
 
 namespace Satusehat\Integration\FHIR;
 
-use Ramsey\Uuid\Uuid;
 use Satusehat\Integration\OAuth2Client;
 
 class Bundle extends OAuth2Client {
+
     public array $bundle = [
         'resourceType' => 'Bundle',
         'type' => 'transaction',
@@ -14,8 +14,24 @@ class Bundle extends OAuth2Client {
 
     public $encounter_id;
 
+    private function uuidV4() {
+        $data = openssl_random_pseudo_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        $uuid = bin2hex($data);
+        $formatted_uuid = sprintf('%s-%s-%s-%s-%s',
+            substr($uuid, 0, 8),
+            substr($uuid, 8, 4),
+            substr($uuid, 12, 4),
+            substr($uuid, 16, 4),
+            substr($uuid, 20, 12)
+        );
+    
+        return $formatted_uuid;
+    }
+
     public function addEncounter(Encounter $encounter){
-        $this->encounter_id = Uuid::uuid4();
+        $this->encounter_id = $this->uuidV4();
         $encounter_bundle = [
             'fullUrl' => 'urn:uuid:' . $this->encounter_id,
             'resource' => json_decode($encounter->json()),
@@ -31,7 +47,7 @@ class Bundle extends OAuth2Client {
     public function addCondition(Condition $condition){
         $condition->setEncounter($this->encounter_id);
         $condition_bundle = [
-            'fullUrl' => 'urn:uuid:' . Uuid::uuid4(),
+            'fullUrl' => 'urn:uuid:' . $this->uuidV4(),
             'resource' => json_decode($condition->json()),
             'request' => [
                 'method' => 'POST',
